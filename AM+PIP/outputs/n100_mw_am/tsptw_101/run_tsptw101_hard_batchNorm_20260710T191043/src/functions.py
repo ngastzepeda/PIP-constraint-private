8da3b7d0602e95f43f0f0b25717f9150cca9ba82
ngastzepeda <n.gastzepeda@outlook.com>
2026-07-22 14:05:24 +0200

    AM+PIP n100_mw_am outputs

---
+++ AM+PIP/outputs/n100_mw_am/tsptw_101/run_tsptw101_hard_batchNorm_20260710T191043/src/functions.py
@@ -0,0 +1,361 @@
+import warnings
+
+import torch
+import numpy as np
+import os
+import json
+from tqdm import tqdm
+from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing import Pool
+import torch.nn.functional as F
+import random
+import sys
+import logging
+import shutil
+
+class AverageMeter:
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.sum += (val * n)
+        self.count += n
+
+    @property
+    def avg(self):
+        return self.sum / self.count if self.count else 0
+
+def load_problem(name):
+    from problems import TSPTW
+    problem = {
+        'tsptw': TSPTW,
+    }.get(name, None)
+    assert problem is not None, "Currently unsupported problem: {}!".format(name)
+    return problem
+
+
+def torch_load_cpu(load_path):
+    # weights_only=False: checkpoints contain full baseline state (model/dataset objects)
+    return torch.load(load_path, map_location=lambda storage, loc: storage, weights_only=False)  # Load on CPU
+
+
+def move_to(var, device):
+    if isinstance(var, dict):
+        return {k: move_to(v, device) for k, v in var.items()}
+    return var.to(device)
+
+
+def _load_model_file(load_path, model):
+    """Loads the model with parameters from the file and returns optimizer state dict if it is in the file"""
+
+    # Load the model parameters from a saved state
+    load_optimizer_state_dict = None
+    print('  [*] Loading model from {}'.format(load_path))
+
+    load_data = torch.load(
+        os.path.join(
+            os.getcwd(),
+            load_path
+        ), map_location=lambda storage, loc: storage, weights_only=False)
+
+    if isinstance(load_data, dict):
+        load_optimizer_state_dict = load_data.get('optimizer', None)
+        load_model_state_dict = load_data.get('model', load_data)
+    else:
+        load_model_state_dict = load_data.state_dict()
+
+    state_dict = model.state_dict()
+
+    state_dict.update(load_model_state_dict)
+
+    model.load_state_dict(state_dict, strict=False)
+
+    return model, load_optimizer_state_dict
+
+
+def load_args(filename):
+    with open(filename, 'r') as f:
+        args = json.load(f)
+
+    # Backwards compatibility
+    if 'data_distribution' not in args:
+        args['data_distribution'] = None
+        probl, *dist = args['problem'].split("_")
+        if probl == "op":
+            args['problem'] = probl
+            args['data_distribution'] = dist[0]
+    return args
+
+
+def load_model(path, epoch=None):
+    from nets.attention_model import AttentionModel
+    from nets.pointer_network import PointerNetwork
+
+    if os.path.isfile(path):
+        model_filename = path
+        path = os.path.dirname(model_filename)
+    elif os.path.isdir(path):
+        if epoch is None:
+            epoch = max(
+                int(os.path.splitext(filename)[0].split("-")[1])
+                for filename in os.listdir(path)
+                if os.path.splitext(filename)[1] == '.pt'
+            )
+        model_filename = os.path.join(path, 'epoch-{}.pt'.format(epoch))
+    else:
+        assert False, "{} is not a valid directory or file".format(path)
+
+    args = load_args(os.path.join(path, 'args.json'))
+
+    problem = load_problem(args['problem'])
+
+    model_class = {
+        'attention': AttentionModel,
+        'pointer': PointerNetwork
+    }.get(args.get('model', 'attention'), None)
+    assert model_class is not None, "Unknown model: {}".format(model_class)
+
+    print("generate_PI_mask: ",args['generate_PI_mask'])
+    try:
+        print("pip_decoder: ", args['pip_decoder'])
+    except:
+        args['pip_decoder'] = False
+        args['sl_loss'] = "BCEWithLogitsLoss"
+        args['decision_boundary'] = 0.5
+        pass
+    print("normalization: ", args['normalization'])
+    model = model_class(
+        args['embedding_dim'],
+        args['hidden_dim'],
+        problem,
+        generate_PI_mask=args['generate_PI_mask'],
+        n_encode_layers= args['n_encode_layers'],
+        pip_decoder= args['pip_decoder'],
+        sigmoid= False if args['sl_loss'] == "BCEWithLogitsLoss" else True,
+        decision_boundary= args['decision_boundary'],
+        mask_inner=True,
+        mask_logits=True,
+        normalization=args['normalization'],
+        tanh_clipping=args['tanh_clipping'],
+        checkpoint_encoder=args.get('checkpoint_encoder', False),
+        shrink_size=args.get('shrink_size', None),
+        include_service_time=args.get('include_service_time', False)
+    )
+    # Overwrite model parameters by parameters to load
+    load_data = torch_load_cpu(model_filename)
+    model.load_state_dict({**model.state_dict(), **load_data.get('model', {})}, strict=False)
+    model.eval() # Put in eval mode
+    model, *_ = _load_model_file(model_filename, model)
+
+    return model, args
+
+
+def parse_softmax_temperature(raw_temp):
+    # Load from file
+    if os.path.isfile(raw_temp):
+        return np.loadtxt(raw_temp)[-1, 0]
+    return float(raw_temp)
+
+
+def run_all_in_pool(func, directory, dataset, opts, use_multiprocessing=True):
+    # # Test
+    # res = func((directory, 'test', *dataset[0]))
+    # return [res]
+
+    num_cpus = os.cpu_count() if opts.cpus is None else opts.cpus
+
+    w = len(str(len(dataset) - 1))
+    offset = getattr(opts, 'offset', None)
+    if offset is None:
+        offset = 0
+    ds = dataset[offset:(offset + opts.n if opts.n is not None else len(dataset))]
+    pool_cls = (Pool if use_multiprocessing and num_cpus > 1 else ThreadPool)
+    with pool_cls(num_cpus) as pool:
+        results = list(tqdm(pool.imap(
+            func,
+            [
+                (
+                    directory,
+                    str(i + offset).zfill(w),
+                    *problem
+                )
+                for i, problem in enumerate(ds)
+            ]
+        ), total=len(ds), mininterval=opts.progress_bar_mininterval))
+
+    failed = [str(i + offset) for i, res in enumerate(results) if res is None]
+    assert len(failed) == 0, "Some instances failed: {}".format(" ".join(failed))
+    return results, num_cpus
+
+
+def do_batch_rep(v, n):
+    if isinstance(v, dict):
+        return {k: do_batch_rep(v_, n) for k, v_ in v.items()}
+    elif isinstance(v, list):
+        return [do_batch_rep(v_, n) for v_ in v]
+    elif isinstance(v, tuple):
+        return tuple(do_batch_rep(v_, n) for v_ in v)
+
+    return v[None, ...].expand(n, *v.size()).contiguous().view(-1, *v.size()[1:])
+
+
+def sample_many(inner_func, get_cost_func, input, batch_rep=1, iter_rep=1):
+    """
+    :param input: (batch_size, graph_size, node_dim) input node features
+    :return:
+    """
+    input = do_batch_rep(input, batch_rep)
+
+    costs = []
+    pis = []
+    for i in range(iter_rep):
+        _, pi = inner_func(input)
+        # pi.view(-1, batch_rep, pi.size(-1))
+        cost, mask = get_cost_func(input, pi)
+
+        costs.append(cost.view(batch_rep, -1).t())
+        pis.append(pi.view(batch_rep, -1, pi.size(-1)).transpose(0, 1))
+
+    max_length = max(pi.size(-1) for pi in pis)
+    # (batch_size * batch_rep, iter_rep, max_length) => (batch_size, batch_rep * iter_rep, max_length)
+    pis = torch.cat(
+        [F.pad(pi, (0, max_length - pi.size(-1))) for pi in pis],
+        1
+    )  # .view(embeddings.size(0), batch_rep * iter_rep, max_length)
+    costs = torch.cat(costs, 1)
+    mask = mask.sum(-1).view(batch_rep, -1).t()
+
+    mincost, minpi = [], []
+    # (batch_size)
+    for i in range(costs.size(0)):
+        if (mask[i]==0).any(): # have feasible
+            min_cost, min_idx = costs[i][mask[i]==0].min(-1)
+            mincost.append(min_cost)
+            minpi.append(pis[i][mask[i]==0][min_idx.item()])
+        else: # no feasible
+            mincost.append(float('inf'))
+            minpi.append(torch.arange(1, pi.size(-1)+1).cuda())
+    mincosts = torch.tensor(mincost)
+    minpis = torch.stack(minpi)
+    # mincosts, argmincosts = costs.min(-1)
+    # # (batch_size, minlength)
+    # minpis = pis[torch.arange(pis.size(0), out=argmincosts.new()), argmincosts]
+    ins_infsb_num = (mask!=0).all(-1).int()
+    sol_infsb_num = (mask!=0).sum(-1)
+
+    return minpis, mincosts, ins_infsb_num, sol_infsb_num
+
+def accurate_cdist(x1, x2):
+    # Do not use matrix multiplication since this is inaccurate
+    return torch.cdist(x1, x2, compute_mode='donot_use_mm_for_euclid_dist')
+
+
+def seed_everything(seed=2023):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
+    torch.cuda.manual_seed_all(seed)
+
+class StreamToLogger(object):
+    """
+    Fake file-like stream object that redirects writes to a logger instance.
+    """
+
+    def __init__(self, logger, log_level=logging.INFO):
+        self.logger = logger
+        self.log_level = log_level
+
+    def write(self, buf):
+        for line in buf.rstrip().splitlines():
+            self.logger.log(self.log_level, line.rstrip())
+
+    def flush(self):
+        pass  # This flush method is required for file-like object.
+
+def create_logger(filename, log_path=None):
+    if log_path and not os.path.exists(log_path):
+        os.makedirs(log_path)
+
+    file_mode = 'a' if os.path.isfile(os.path.join(log_path, filename)) else 'w'
+
+    root_logger = logging.getLogger()
+    root_logger.setLevel(level=logging.INFO)
+    formatter = logging.Formatter("[%(asctime)s] %(filename)s(%(lineno)d) : %(message)s", "%Y-%m-%d %H:%M:%S")
+
+    # Clear existing handlers
+    for hdlr in root_logger.handlers[:]:
+        root_logger.removeHandler(hdlr)
+
+    # Write to file
+    fileout = logging.FileHandler(os.path.join(log_path, filename), mode=file_mode)
+    fileout.setLevel(logging.INFO)
+    fileout.setFormatter(formatter)
+    root_logger.addHandler(fileout)
+
+    # Write to console
+    console = logging.StreamHandler(sys.stdout)
+    console.setLevel(logging.INFO)
+    console.setFormatter(formatter)
+    root_logger.addHandler(console)
+
+    # Redirect print to logging
+    stdout_logger = logging.getLogger('STDOUT')
+    sl = StreamToLogger(stdout_logger, logging.INFO)
+    sys.stdout = sl
+
+def copy_all_src(dst_root):
+
+    # execution dir
+    if os.path.basename(sys.argv[0]).startswith('ipykernel_launcher'):
+        execution_path = os.getcwd()
+    else:
+        execution_path = os.path.dirname(sys.argv[0])
+
+    # home dir setting
+    tmp_dir1 = os.path.abspath(os.path.join(execution_path, sys.path[0]))
+    tmp_dir2 = os.path.abspath(os.path.join(execution_path, sys.path[1]))
+
+    if len(tmp_dir1) > len(tmp_dir2) and os.path.exists(tmp_dir2):
+        home_dir = tmp_dir2
+    else:
+        home_dir = tmp_dir1
+
+    # make target directory
+    dst_path = os.path.join(dst_root, 'src')
+
+    if not os.path.exists(dst_path):
+        os.makedirs(dst_path)
+
+    for item in sys.modules.items():
+        key, value = item
+
+        if hasattr(value, '__file__') and value.__file__:
+            src_abspath = os.path.abspath(value.__file__)
+
+            # skip non-existent files
+            if not os.path.exists(src_abspath):
+                print(f"[copy_all_src] Warning: {src_abspath} not found. Skipping.")
+                continue
+
+            if os.path.commonprefix([home_dir, src_abspath]) == home_dir:
+                dst_filepath = os.path.join(dst_path, os.path.basename(src_abspath))
+
+                if os.path.exists(dst_filepath):
+                    split = list(os.path.splitext(dst_filepath))
+                    split.insert(1, '({})')
+                    filepath = ''.join(split)
+                    post_index = 0
+
+                    while os.path.exists(filepath.format(post_index)):
+                        post_index += 1
+
+                    dst_filepath = filepath.format(post_index)
+
+                shutil.copy(src_abspath, dst_filepath)
